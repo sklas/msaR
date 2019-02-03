998cff88090d61ea7e110da643d7c7a93ade4953
zach charlop-powers <zach.charlop.powers@gmail.com>
2019-02-02 22:57:29 -0500

    begin conversion to the new HTMLWidgets format

--- inst/htmlwidgets/msaR.js
+++ inst/htmlwidgets/msaR.js
@@ -3,70 +3,74 @@ HTMLWidgets.widget({
   name: 'msaR',
 
   type: 'output',
-
-  initialize: function(el, width, height) {
-
-    var rootDiv = el;
-
+  
+  factory: function(el, width, height) {
+    
+    // define the object at the root node
+    //m = new msa(el.id)
+    
+    var rootDiv = el.id;
+    var parse   = require("biojs-io-fasta").parse;
+    var opts; 
+    
     return {
-      msa: msa,
-      el: rootDiv,
-      parse: require("biojs-io-fasta").parse
-    };
-  },
+          renderValue: function(x) {
+              
+              // msa is initialized with a DOM element, seqs, config, viz, and zoomer
+              // https://github.com/wilzbach/msa/blob/master/src/msa.js
+              opts      = x.config;
+              opts.el   = rootDiv;
+              console.log(x.alignment);
+              console.log(parse);
+              opts.seqs = parse(x.alignment);
+    
+              var m = msa(opts);
 
-  renderValue: function(el, x, instance) {
-    var opts = x.config;
-    opts.el = instance.el;
-    opts.seqs = instance.parse(x.alignment);
+              // init msa
+              if (x.menu) {
+                // the menu is independent to the MSA container
+                var menuOpts = {};
+                menuOpts.el = document.getElementById('div');
+                menuOpts.msa = m;
+                menuOpts.menu = "small";
+                var defMenu = new msa.menu.defaultmenu(menuOpts);
+                m.addView("menu", defMenu);
+              }
 
-    // init msa
-    var m = instance.msa(opts);
-    var test = m.g.colorscheme.set("scheme", "nucleotide");
+              // save msa instance to window object so it can be hacked using shinyjs
+              if (x.features) {
+                var features = {
+                  config: {
+                    type: "gff3"
+                  },
+                  seqs: {}
+                };
+        
+                for(var i=0; i<x.features.length; i++){
+                  if(features.seqs[x.features[i].seqName] === undefined || features.seqs[x.features[i].seqName] === null){
+                    features.seqs[x.features[i].seqName] = [];
+                  }
+                  features.seqs[x.features[i].seqName].push({
+                    attributes: {
+                      Color: "#4285f4",
+                      Name: "sgRNA"
+                    },
+                    end: x.features[i].end,
+                    feature: "gene",
+                    start: x.features[i].start
+                  });
+                }
+        
+                m.seqs.addFeatures(features);
+              }
+        
+              // call render at the end to display the whole MSA
+              m.render();
+            },
 
-    if(x.menu){
-      // the menu is independent to the MSA container
-      var menuOpts = {};
-      menuOpts.el = document.getElementById('div');
-      menuOpts.msa = m;
-      menuOpts.menu = "small";
-      var defMenu = new msa.menu.defaultmenu(menuOpts);
-      m.addView("menu", defMenu);
-    }
-
-    // save msa instance to window object so it can be hacked using shinyjs
-    if(x.features){
-      var features = {
-        config: {
-          type: "gff3"
-        },
-        seqs: {}
-      };
-
-      for(var i=0; i<x.features.length; i++){
-        if(features.seqs[x.features[i].seqName] === undefined || features.seqs[x.features[i].seqName] === null){
-          features.seqs[x.features[i].seqName] = [];
-        }
-        features.seqs[x.features[i].seqName].push({
-          attributes: {
-            Color: "#4285f4",
-            Name: "sgRNA"
-          },
-          end: x.features[i].end,
-          feature: "gene",
-          start: x.features[i].start
-        });
+    resize: function(width, height) {
+      
       }
-
-      m.seqs.addFeatures(features);
     }
-
-    // call render at the end to display the whole MSA
-    m.render();
-  },
-
-  resize: function(el, width, height, instance) {
-
   }
-
 });
